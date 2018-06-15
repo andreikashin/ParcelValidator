8dbb2378ee775c9e17c787f506746381b8962f0a
andrei <andrei.kashin@gmail.com>
2018-06-15 18:55:19 +0300

    main method and size structures

--- ParcelValidator/Program.cs
+++ ParcelValidator/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace ParcelValidator
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var parcelSize = new ParcelSize();
+
+
+            if (args.Length == 1)
+            {
+                var values = args.FirstOrDefault()?.Split(',');
+
+                if (values == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                if (values.Length < Marshal.SizeOf(typeof(ParcelSize)) +
+                    Marshal.SizeOf(typeof(PipeCorner)))
+                {
+                    throw new ArgumentException("Insufficient arguments");
+                }
+
+                parcelSize.Length = int.Parse(values[0]);
+                parcelSize.Width = int.Parse(values[1]);
+            }
+
+            else if (args.Length > 1)
+            {
+                parcelSize.Length = 1;
+                parcelSize.Width = 1;
+            }
         }
+    }
+
+    internal struct ParcelSize
+    {
+        internal int Length;
+        internal int Width;
+    }
+
+    internal struct PipeCorner
+    {
+        internal int Inlet;
+        internal int Outlet;
     }
 }
