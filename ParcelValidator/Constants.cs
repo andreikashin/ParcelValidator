68c19742f8b418f248112a8ce7e4f51e9a238957
andrei <andrei.kashin@gmail.com>
2018-06-16 03:30:55 +0300

    Parse arguments

---
+++ ParcelValidator/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcelValidator
+{
+    internal static class Constants
+    {
+        public static int ParcelDimensionCount = 2;
+        public static int CornerDimensionCount = 2;
+    }
+}
--- ParcelValidator/Program.cs
+++ ParcelValidator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 
@@ -8,8 +9,9 @@ namespace ParcelValidator
     {
         public static void Main(string[] args)
         {
-            var parcelSize = new ParcelSize();
 
+            var parcelSize = new ParcelSize();
+            var pipeCorners = new List<PipeCorner>();
 
             if (args.Length == 1)
             {
@@ -19,21 +21,38 @@ namespace ParcelValidator
                 {
                     throw new ArgumentNullException();
                 }
-                if (values.Length < Marshal.SizeOf(typeof(ParcelSize)) +
-                    Marshal.SizeOf(typeof(PipeCorner)))
+                if (values.Length < Constants.ParcelDimensionCount +
+                    Constants.CornerDimensionCount)
                 {
                     throw new ArgumentException("Insufficient arguments");
                 }
 
                 parcelSize.Length = int.Parse(values[0]);
                 parcelSize.Width = int.Parse(values[1]);
+                pipeCorners = ParseCorners(values);
             }
 
             else if (args.Length > 1)
             {
-                parcelSize.Length = 1;
-                parcelSize.Width = 1;
+                throw new ArgumentException();
             }
+
+        }
+
+        private static List<PipeCorner> ParseCorners(string[] values)
+        {
+            var result = new List<PipeCorner>();
+
+            for (var idx = 2; idx < values.Length - 1; idx++)
+            {
+                result.Add(new PipeCorner
+                {
+                    Inlet = int.Parse(values[idx]),
+                    Outlet = int.Parse(values[idx + 1])
+                });
+            }
+
+            return result;
         }
     }
 
